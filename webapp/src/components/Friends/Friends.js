ede4824b92c080c55699215538ba8385a64ceeae
uo245020 <uo245020@uniovi.es>
2021-03-12 22:27:13 +0100

    Añadir amigos falta autenticación

--- webapp/src/components/Friends/Friends.js
+++ webapp/src/components/Friends/Friends.js
@@ -4,21 +4,57 @@ import "./Friends.css";
 import "bootstrap/dist/css/bootstrap.css";
 import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
 import DocumentTitle from "react-document-title";
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+const { default: data } = require("@solid/query-ldflex");
 
+
+
+const addFriend = async (idAmigo, id) => {
+    const user = data[id]; 
+   
+      if (idAmigo.localeCompare("") !== 0) {
+        if (await amigoExistente(idAmigo, id)) {
+          toast.error("Ya tienes agregado este amigo", {
+            position: toast.POSITION.BOTTOM_LEFT,
+            autoClose: 5000
+          } );
+        } else {
+        await user.knows.add(data[idAmigo]);
+          toast.info("Tu amigo se ha agregado", {
+            position: toast.POSITION.BOTTOM_LEFT,
+            autoClose: 5000
+          } );
+          await sleep(5000);
+          reload();
+        }
+      } else {
+        toast.error("Cadena vacía", {
+          position: toast.POSITION.BOTTOM_LEFT,
+          autoClose: 5000
+        } );
+      }
+    };
+
+  const amigoExistente = async (idAmigo, id) => {
+    const user = data[id];
+    for await (const friend of user.friends) {
+      if (String(friend).localeCompare(String(idAmigo)) === 0){ return true;}
+    }
+    return false;
+  };
 
 const Friends = () => {
     return (
-      <DocumentTitle title="Friends">
+      <DocumentTitle title="Amigos">
         <div className="prueba">
           <h2 className="h2" data-testId="label">Estos son tus amigos, <Value src="Javier" /> </h2>
           <h4 class="card-title" id="addFriend" data-testId="addFriend">Añade tus amigos</h4>
           <div class="wrap">
             <div class="search">
               <input type="text" class="searchTerm" placeholder="https://usuario.solid.community/profile/card#me" id="input" />
-              <button type="submit" class="searchButton">
+              <button type="submit" class="searchButton" onClick={() => addFriend(document.getElementById("input").value, "https://javigrao.solidcommunity.net/profile/card#me")}>
                 <SearchOutlinedIcon className="iconSearch" />
               </button>
             </div>
@@ -30,5 +66,14 @@ const Friends = () => {
       </DocumentTitle>
     );
   };
+
+
+  const reload = () => {
+    window.location.reload();
+  };
+
+  async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 
   export default Friends;
