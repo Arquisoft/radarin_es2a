69d0b0695ad57db419e913fd07f99ba37a6c6842
Lucas <UO264423@uniovi.es>
2021-03-12 22:43:01 +0100

    Prototipo de mapa sin marcadores

---
+++ webapp/src/components/MapContainer.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+import credentials from '../api/credentials'
+
+const mapStyles = {
+    width: '100%',
+    height: '100%'
+  };
+  
+  export class MapContainer extends Component {
+    render() {
+      return (
+        <Map
+          google={this.props.google}
+          zoom={14}
+          style={mapStyles}
+          initialCenter={
+            {
+              lat: -1.2884,
+              lng: 36.8233
+            }
+          }
+        />
+      );
+    }
+  }
+  
+  export default GoogleApiWrapper({
+    apiKey: `${credentials.mapsKey}`
+  })(MapContainer);
