524af996918b20b440c9c3eb2c9a6e42238c13a6
uo245020 <uo245020@uniovi.es>
2021-03-13 10:25:48 +0100

    Listado de amigos beta

--- webapp/src/components/Friends/Friends.js
+++ webapp/src/components/Friends/Friends.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Value } from "@solid/react";
+import {  List, Value, Name, Link } from "@solid/react";
 import "./Friends.css";
 import "bootstrap/dist/css/bootstrap.css";
 import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
@@ -59,6 +59,15 @@ const Friends = () => {
               </button>
             </div>
           </div>
+
+          <br></br>
+        <List src={`[${"https://javigrao.solidcommunity.net/profile/card#me"}].friends`} className="list" padding-inline-start="0">{(friend) =>
+          <li key={friend} className="listElement">
+            <p>
+              <Card nombre={`[${friend}]`} web={"https://javigrao.solidcommunity.net/profile/card#me"}></Card>
+            </p>
+          </li>}
+        </List>
   
           <br></br>
           <ToastContainer />
@@ -75,5 +84,24 @@ const Friends = () => {
   async function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
+
+
+  const Card = (props) => {
+    return (
+      <div class="card bg-info text-white" >
+        <div class="card-body">
+          <h2 class="card-title" id="friendName">
+            <Name src={props.nombre}>{props.nombre}</Name>
+          </h2>
+          <center>
+            <div className="botones">
+              <Link href={props.nombre} className="btn btn-light" id="botonOpcion" data-testId="link">Perfil</Link>
+              <button className="btn btn-light" id="botonOpcion" data-testId="button"  >Eliminar</button>
+            </div>
+          </center>
+        </div>
+      </div>
+    );
+  };
 
   export default Friends;
