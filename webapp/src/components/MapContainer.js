09329a561f4f9ee1076a90ed8bfdc6972826c831
Laura <uo271314@uniovi.es>
2021-03-14 17:27:30 +0100

    Incluir nombre de los amigos

--- webapp/src/components/MapContainer.js
+++ webapp/src/components/MapContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper, InfoWindow, Marker, Circle } from 'google-maps-react';
+import { GoogleApiWrapper, InfoWindow, Marker, Circle } from 'google-maps-react';
 import credentials from '../api/credentials'
 import MyLocation from './MyLocation'
 
@@ -17,7 +17,9 @@ MyLocation.defaultProps = {
 
 const coords = { lat: 43.355008507725366, lng: -5.851265172858888 };
 const coords1 = { lat: 43.357786, lng: -5.847369 };
-const amigos = [coords,coords1];
+const amigo1 = {c:coords, name:'Amigo1'};
+const amigo2 = {c:coords1, name:'Amigo2'};
+const amigos = [amigo1,amigo2];
 
 
 export class MapContainer extends Component {
@@ -27,6 +29,14 @@ export class MapContainer extends Component {
     activeMarker: {},          // Muestra el marcador activo al hacer clic
     selectedPlace: {}          // Muestra la InfoWindow del lugar seleccionado sobre un marcador
   };
+
+   // Evento que muestra la InfoWindow al hacer clic sobre el circulo
+   onCircleClick = (props, circle, e) =>
+   this.setState({
+     selectedPlace: props,
+     activeMarker: circle,
+     showingInfoWindow: true
+   });
   
   // Evento que muestra la InfoWindow al hacer clic sobre el marcador
   onMarkerClick = (props, marker, e) =>
@@ -57,15 +67,14 @@ export class MapContainer extends Component {
           amigos.map((value, index) =>{
             return(<Circle
               radius={200}
-              center={value}
-              onMouseover={() => console.log('mouseover')}
-              onClick={() => console.log('click')}
-              onMouseout={() => console.log('mouseout')}
+              center={value.c}
+              onClick={this.onCircleClick}
               strokeColor='transparent'
               strokeOpacity={0}
               strokeWeight={5}
               fillColor='#FF0000'
               fillOpacity={0.2}
+              name={value.name}
             />
             )
           })
@@ -80,7 +89,7 @@ export class MapContainer extends Component {
           onClose={this.onClose}
         >
           <div>
-            <h4>{this.state.selectedPlace.name}</h4>
+            <h6>{this.state.selectedPlace.name}</h6>
           </div>
         </InfoWindow>
       </MyLocation>
