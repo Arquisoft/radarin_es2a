a1b9e0d87348feaa27ed059f8b933126952e1630
uo245020 <uo245020@uniovi.es>
2021-03-29 13:57:47 +0200

    Primera versión del registro de usuarios con firebase

--- webapp/src/components/Registrer.js
+++ webapp/src/components/Registrer.js
@@ -6,14 +6,43 @@ import {db} from '../api/firebase'
 
 function Registrer() {
 
-    const [user] = useState({ name: "", email: "", pod: "" });
     var registro = false;
 
-    const addUser = (details) => {
-       db.collection('users').doc().set(details);
-       const query2 = db.collection('users').get();
-       console.log(Array.from(query2))
+    const addUser = async (details) => {
+        if (details.email.length ===0 || details.password.length ===0 || details.pod.length === 0){
+            console.log("No pueden existir campos vacios")
+        }
+        else{
+        if (details.password.length < 5 ){
+            console.log("La contraseña ha de tener mínimo 5 caracteres")
+        }
+        else{
+        if(await existeUsuario(details.email)===false){
+            await db.collection('users').doc().set(details);
+            console.log("Usuario añadido correctamente")
+        }
+        else{
+        console.log("Ya existe un usuario con esa dirección de correo")
+        }
     }
+    }
+    }
+
+    const existeUsuario = async (emailUsuario) => {
+        const querySnapShot = await db.collection('users').get();
+        var existeUsuario = false;
+        querySnapShot.forEach(doc => {
+            if (String(doc.data().email.localeCompare(emailUsuario))=== String(0)){
+                existeUsuario = true;
+                }
+        })
+        if (existeUsuario){
+            return true;
+        }
+        else{
+            return false;
+        }
+      };
 
     if (registro) {
         return (
--- webapp/src/index.js
+++ webapp/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {FirebaseAppProvider} from 'reactfire';
 import reportWebVitals from './reportWebVitals';
 import { Suspense } from 'react';
 
