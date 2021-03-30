64e0ad05331367946dd3961933277cd1997a6fb0
Lucas <UO264423@uniovi.es>
2021-03-30 06:58:29 +0200

    Barra de navegacion temporal

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -4,56 +4,44 @@ import logo from './logo.svg';
 import Welcome from './components/Welcome';
 //import LoginConstants from "./components/LoginConstants";
 import Login from "./components/Login";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Registrer from './components/Registrer';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import NavBar from './components/Navigation/Navbar';
+import { MapContainer } from './components/MapContainer';
+import Friends from './components/Friends/Friends';
 
 
-class App extends React.Component{
-  constructor(){
+class App extends React.Component {
+  constructor() {
     super()
-    this.state = {users:[]}
+    this.state = { users: [] }
   }
 
-  refreshUsers(users){
-    this.setState({users:users})
+  refreshUsers(users) {
+    this.setState({ users: users })
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <Welcome name="Bienvenid@"/>
-        </header>
-        <div className="App-content">
-          <Login />
-          <ToastContainer/>
-        </div>
         <Router>
-
-<br></br>
-<Link to='/registrarse'>Registrarse</Link>
-
-  <Route path="/registrarse" render={() =>{
-    return(
-      <div>
-        <Registrer/>
-        <ToastContainer/>
+          <NavBar/>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <Welcome name="Bienvenid@" />
+          </header>
+          <div className="App-content">
+            <Route path='/login' component={Login} />
+            <Route path="/registrarse" component={Registrer} />
+            <Route path="/amigos" component={Friends} />
+          </div>
+        </Router>
       </div>
-    )
-  }}>
-  </Route>
-  
-</Router>
-
-      </div>
-      
 
 
-      
     )
   }
 }
