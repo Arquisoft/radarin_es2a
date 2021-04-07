267c4c13c44b899a46d6f95d18c1c3379cea67d5
uo245020 <uo245020@uniovi.es>
2021-04-07 17:25:54 +0200

    Listado de amigos funcionando

--- webapp/src/components/Friends/Friends.js
+++ webapp/src/components/Friends/Friends.js
@@ -1,119 +1,141 @@
-import React, {useState} from "react";
-import {  List, Value, Name, Link } from "@solid/react";
+import React, { useState, useEffect } from "react";
+import { List, Value, Name, Link } from "@solid/react";
 import "./Friends.css";
 import "bootstrap/dist/css/bootstrap.css";
 import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
 import DocumentTitle from "react-document-title";
-import { ToastContainer, toast} from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import {db} from '../../api/firebase'
+import { db } from '../../api/firebase'
+
 
 
 function Friends() {
 
-const { default: data } = require("@solid/query-ldflex");
+  const { default: data } = require("@solid/query-ldflex");
 
-const [details, setDetails] = useState({emisor: window.sessionStorage.getItem('user'), receptor: ""});
+  const usuarioActivo = window.sessionStorage.getItem('user');
 
-const addFriend = async (idAmigo) => {
+  const [details, setDetails] = useState({ emisor: window.sessionStorage.getItem('user'), receptor: "" });
 
-      if (idAmigo.localeCompare(window.sessionStorage.getItem('user')) !== 0 ){
-         if (idAmigo.localeCompare("") !== 0) {
-           if (await existeUsuario(idAmigo)){
-            if (await existeAmigo(idAmigo)) {
-               toast.error("Ya sois amig@s", {
-                position: toast.POSITION.BOTTOM_LEFT,
-                autoClose: 2500
-          } );
-        }  else {
-           if (await existePeticion(idAmigo)){
-            toast.error("Ya habías enviado esta petición", {
+  const [amigos, setAmigos] = useState([]);
+
+
+
+
+  const getAmigos = async () => {
+    db.collection("amigos").onSnapshot((querySnapShot) => {
+      const docs = [];
+      querySnapShot.forEach(doc => {
+        if (String(doc.data().usuario1.localeCompare(usuarioActivo)) === String(0)) {
+          docs.push({ nombre: doc.data().usuario2, id: doc.id })
+        }
+        if ((String(doc.data().usuario2.localeCompare(usuarioActivo)) === String(0))) {
+          docs.push({ nombre: doc.data().usuario1, id: doc.id })
+        }
+      });
+      setAmigos(docs);
+    });
+  };
+
+  const addFriend = async (idAmigo) => {
+    if (idAmigo.localeCompare(window.sessionStorage.getItem('user')) !== 0) {
+      if (idAmigo.localeCompare("") !== 0) {
+        if (await existeUsuario(idAmigo)) {
+          if (await existeAmigo(idAmigo)) {
+            toast.error("Ya sois amig@s", {
               position: toast.POSITION.BOTTOM_LEFT,
               autoClose: 2500
-        } );
-           }
-           else{
-          await db.collection('peticiones').doc().set(details);
-          toast.info("Has enviado la petición de amistad correctamente", {
+            });
+          } else {
+            if (await existePeticion(idAmigo)) {
+              toast.error("Ya habías enviado esta petición", {
+                position: toast.POSITION.BOTTOM_LEFT,
+                autoClose: 2500
+              });
+            }
+            else {
+              await db.collection('peticiones').doc().set(details);
+              toast.info("Has enviado la petición de amistad correctamente", {
+                position: toast.POSITION.BOTTOM_LEFT,
+                autoClose: 2500
+              });
+              await sleep(2500);
+            }
+          }
+        }
+        else {
+          toast.error("No existe el usuario indicado", {
             position: toast.POSITION.BOTTOM_LEFT,
             autoClose: 2500
-          } );
-          await sleep(2500);
+          });
         }
-        }
-      }
-      else{
-        toast.error("No existe el usuario indicado", {
-          position: toast.POSITION.BOTTOM_LEFT,
-          autoClose: 2500
-    } );
-      }
       } else {
         toast.error("Cadena vacía", {
           position: toast.POSITION.BOTTOM_LEFT,
           autoClose: 2500
-        } );
+        });
       }
     }
-   else{
-    toast.error("No puedes agregarte a ti mism@", {
-      position: toast.POSITION.BOTTOM_LEFT,
-      autoClose: 2500
-    } );
-  }
-    };
+    else {
+      toast.error("No puedes agregarte a ti mism@", {
+        position: toast.POSITION.BOTTOM_LEFT,
+        autoClose: 2500
+      });
+    }
+  };
 
   const existeAmigo = async (idAmigo) => {
     const querySnapShot = await db.collection('amigos').get();
     var existeAmigo = false;
     querySnapShot.forEach(doc => {
-        if (String(doc.data().usuario1.localeCompare(details.emisor))=== String(0) && (String(doc.data().usuario2.localeCompare(idAmigo))=== String(0))){
-            existeAmigo = true;
-            }
-            if (String(doc.data().usuario2.localeCompare(details.emisor))=== String(0) && (String(doc.data().usuario1.localeCompare(idAmigo))=== String(0))){
-              existeAmigo = true;
-              }   
+      if (String(doc.data().usuario1.localeCompare(details.emisor)) === String(0) && (String(doc.data().usuario2.localeCompare(idAmigo)) === String(0))) {
+        existeAmigo = true;
+      }
+      if (String(doc.data().usuario2.localeCompare(details.emisor)) === String(0) && (String(doc.data().usuario1.localeCompare(idAmigo)) === String(0))) {
+        existeAmigo = true;
+      }
     })
-    if (existeAmigo){
-        return true;
+    if (existeAmigo) {
+      return true;
     }
-    else{
-        return false;
+    else {
+      return false;
     }
   };
 
-  const existePeticion= async (idAmigo) => {
+  const existePeticion = async (idAmigo) => {
     const querySnapShot = await db.collection('peticiones').get();
-        var existePeticion = false;
-        querySnapShot.forEach(doc => {
-            if (String(doc.data().emisor.localeCompare(details.emisor))=== String(0) && (String(doc.data().receptor.localeCompare(idAmigo))=== String(0))){
-                existePeticion = true;
-                }
-        })
-        if (existePeticion){
-            return true;
-        }
-        else{
-            return false;
-        }
-      };
-  
+    var existePeticion = false;
+    querySnapShot.forEach(doc => {
+      if (String(doc.data().emisor.localeCompare(details.emisor)) === String(0) && (String(doc.data().receptor.localeCompare(idAmigo)) === String(0))) {
+        existePeticion = true;
+      }
+    })
+    if (existePeticion) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  };
+
 
   const existeUsuario = async (idAmigo) => {
     const querySnapShot = await db.collection('users').get();
-        var existeUsuario = false;
-        querySnapShot.forEach(doc => {
-            if (String(doc.data().email.localeCompare(idAmigo))=== String(0)){
-                existeUsuario = true;
-                }
-        })
-        if (existeUsuario){
-            return true;
-        }
-        else{
-            return false;
-        }
-      };
+    var existeUsuario = false;
+    querySnapShot.forEach(doc => {
+      if (String(doc.data().email.localeCompare(idAmigo)) === String(0)) {
+        existeUsuario = true;
+      }
+    })
+    if (existeUsuario) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  };
 
 
 
@@ -121,57 +143,63 @@ const addFriend = async (idAmigo) => {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
+  useEffect(() => {
+    getAmigos();
+  }, []);
 
-  const Card = (props) => {
+  if (window.sessionStorage.getItem('user') !== null) {
     return (
-      <div class="card bg-info text-white" >
-        <div class="card-body">
-          <h2 class="card-title" id="friendName">
-            <Name src={props.nombre}>{props.nombre}</Name>
-          </h2>
-          <center>
-            <div className="botones">
-              <Link href={props.nombre} className="btn btn-light" id="botonOpcion" data-testId="link">Perfil</Link>
-              <button className="btn btn-light" id="botonOpcion" data-testId="button"  >Eliminar</button>
+      <DocumentTitle title="Amigos">
+        <div className="prueba">
+          <h2 className="h2" data-testId="label">Estos son tus amig@s: </h2>
+          <h4 class="card-title" id="addFriend" data-testId="addFriend">Envía una petición a un/a amig@</h4>
+          <div class="wrap">
+            <div class="search">
+              <input type="text" class="searchTerm" placeholder="correoamigo@amigosparasiempre.es" onChange={e => setDetails({ ...details, receptor: e.target.value })} id="input" />
+              <button type="submit" class="searchButton" onClick={() => addFriend(document.getElementById("input").value)}>
+                <SearchOutlinedIcon className="iconSearch" />
+              </button>
             </div>
-          </center>
-        </div>
-      </div>
-    );
-  };
-
-  
-  return (
-    <DocumentTitle title="Amigos">
-      <div className="prueba">
-      <h2 className="h2" data-testId="label">Estos son tus amig@s: </h2>
-        <h4 class="card-title" id="addFriend" data-testId="addFriend">Envía una petición a un/a amig@</h4>
-        <div class="wrap">
-          <div class="search">
-            <input type="text" class="searchTerm" placeholder="correoamigo@amigosparasiempre.es" onChange={e => setDetails({...details, receptor:e.target.value})} id="input" />
-            <button type="submit" class="searchButton"  onClick={() => addFriend(document.getElementById("input").value)}>
-              <SearchOutlinedIcon className="iconSearch" />
-            </button>
           </div>
+
+
+
+          <br></br>
+          <div className="col-md-16 p-2">
+          
+            {amigos.map(amigo => (
+              
+              <div class="card bg-info text-white" >
+                
+                <div class="card-body">
+                  <h2 class="card-title" id="friendName">
+                    
+                    <Name src={amigo.nombre}>{amigo.nombre}</Name>
+                  </h2>
+                  <center>
+                    <div className="botones p-2">
+                      <button className="btn btn-light" id="botonOpcion" /*onClick={() => chatear(usuarioActivo, amigo)} */ data-testId="btnChatear" >Chatear</button>
+                      <button className="btn btn-light" id="botonOpcion" /*onClick={() => verUbicación(amigo)} */ data-testId="btnUbicacion"  >Ver ubicación </button>
+                      <button className="btn btn-light" id="botonOpcion" /*onClick={() => eliminarAmigo(amigo)} */ data-testId="btnEliminar"  >Eliminar </button>
+                    </div>
+                  </center>
+                </div>
+              </div>
+            )
+            
+            )}
+            
+          </div>
+          <br></br>
         </div>
+      </DocumentTitle>
+    );
+  }
+  else {
+    //Redireccionar a login
 
-        
-
-        <br></br>
-      <List className="list" padding-inline-start="0">{(friend) =>
-        <li key={friend} className="listElement">
-          <p>
-            <Card nombre={`[${friend}]`} web={"https://javigrao.solidcommunity.net/profile/card#me"}></Card>
-          </p>
-        </li>}
-      </List>
-
-        <br></br>
-      </div>
-    </DocumentTitle>
-  );
-
+  }
 
 }
 
-  export default Friends;
+export default Friends;
--- webapp/src/components/Friends/Peticiones.js
+++ webapp/src/components/Friends/Peticiones.js
@@ -1,96 +1,99 @@
-import React, {useState, useEffect} from "react";
-import {  List, Value, Name, Link } from "@solid/react";
+import React, { useState, useEffect } from "react";
+import { List, Value, Name, Link } from "@solid/react";
 import "./Friends.css";
 import "bootstrap/dist/css/bootstrap.css";
 import DocumentTitle from "react-document-title";
-import { ToastContainer, toast} from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import {db} from '../../api/firebase'
+import { db } from '../../api/firebase'
 
 
 function Peticiones() {
 
 
 
-const { default: data } = require("@solid/query-ldflex");
+    const { default: data } = require("@solid/query-ldflex");
 
-const usuarioActivo = window.sessionStorage.getItem('user');
+    const usuarioActivo = window.sessionStorage.getItem('user');
 
-const [peticiones, setPeticiones] = useState([]);
+    const [peticiones, setPeticiones] = useState([]);
 
-const [amigos, setAmigos] = useState({usuario1: window.sessionStorage.getItem('user'), usuario2: ""});
+    const [amigos, setAmigos] = useState({ usuario1: window.sessionStorage.getItem('user'), usuario2: "" });
 
-const getPeticiones= async () => {
-    db.collection("peticiones").onSnapshot((querySnapShot) => {
-        const docs = [];
-        querySnapShot.forEach(doc => {
-            if (String(doc.data().receptor.localeCompare(usuarioActivo))=== String(0)){
-                docs.push({...doc.data(), id: doc.id})
-            }
+    const getPeticiones = async () => {
+        db.collection("peticiones").onSnapshot((querySnapShot) => {
+            const docs = [];
+            querySnapShot.forEach(doc => {
+                if (String(doc.data().receptor.localeCompare(usuarioActivo)) === String(0)) {
+                    docs.push({ ...doc.data(), id: doc.id })
+                }
+            });
+            setPeticiones(docs);
         });
-        setPeticiones(docs);
-    });
-};
-    
+    };
 
-const aceptarPeticion= async (id,idUsuario) => {
-    await db.collection('peticiones').doc(id).delete();
-    setAmigos(amigos.usuario2=idUsuario);
-    await db.collection('amigos').doc().set(amigos);
-    toast.info("Has aceptado la petición de amistad", {
-        position: toast.POSITION.BOTTOM_LEFT,
-        autoClose: 2500
-      } );
-      };
-  
 
-  const rechazarPeticion = async (id) => {
-    if (window.confirm("¿Estás seguro de rechazar esta petición?")){
+    const aceptarPeticion = async (id, idUsuario) => {
         await db.collection('peticiones').doc(id).delete();
-        toast.info("Has eliminado la petición de amistad correctamente", {
+        setAmigos(amigos.usuario2 = idUsuario);
+        await db.collection('amigos').doc().set(amigos);
+        toast.info("Has aceptado la petición de amistad", {
             position: toast.POSITION.BOTTOM_LEFT,
             autoClose: 2500
-          } );
-    }
-    //await sleep(2500);
-    //reload();
-      };
+        });
+    };
+
+
+    const rechazarPeticion = async (id) => {
+        if (window.confirm("¿Estás seguro de rechazar esta petición?")) {
+            await db.collection('peticiones').doc(id).delete();
+            toast.info("Has eliminado la petición de amistad correctamente", {
+                position: toast.POSITION.BOTTOM_LEFT,
+                autoClose: 2500
+            });
+        }
+        //await sleep(2500);
+        //reload();
+    };
 
     async function sleep(ms) {
-     return new Promise(resolve => setTimeout(resolve, ms));
+        return new Promise(resolve => setTimeout(resolve, ms));
     }
 
 
-  useEffect(() => {
-    getPeticiones();
-  }, []);
+    useEffect(() => {
+        getPeticiones();
+    }, []);
 
-  
 
-  
-  return (
-    <DocumentTitle title="Peticiones">
-      <div className="prueba">
-      <h2 className="h2" data-testId="label">Estas son tus peticiones pendientes: </h2>
-    <div className="col-md-8 p-2">
-        {peticiones.map(peticion => (
-            <div className="card mb-1">
-                <div className="card-body">
-            <h4>{peticion.emisor}</h4>
-            <button className="btn btn-light"  id="botonOpcion"  onClick={() => aceptarPeticion(peticion.id,peticion.emisor)} data-testId="button" >Aceptar</button>
-            <button className="btn btn-light" id="botonOpcion"  onClick={() => rechazarPeticion(peticion.id)}  data-testId="button"  >Rechazar </button>
-            </div>
-            </div>
-        )
-            
-        )}
-    </div>
-    </div>
-        {}
-    </DocumentTitle>
-  );
 
+    if (window.sessionStorage.getItem('user') !== null) {
+        return (
+            <DocumentTitle title="Peticiones">
+                <div className="prueba">
+                    <h2 className="h2" data-testId="label">Estas son tus peticiones pendientes: </h2>
+                    <div className="col-md-8 p-2">
+                        {peticiones.map(peticion => (
+                            <div className="card mb-1">
+                                <div className="card-body">
+                                    <h4>{peticion.emisor}</h4>
+                                    <button className="btn btn-light" id="botonOpcion" onClick={() => aceptarPeticion(peticion.id, peticion.emisor)} data-testId="button" >Aceptar</button>
+                                    <button className="btn btn-light" id="botonOpcion" onClick={() => rechazarPeticion(peticion.id)} data-testId="button"  >Rechazar </button>
+                                </div>
+                            </div>
+                        )
+
+                        )}
+                    </div>
+                </div>
+                {}
+            </DocumentTitle>
+        );
+    }
+    else {
+        //Redireccionar a login
+    }
 
 }
 
-  export default Peticiones;
+export default Peticiones;
