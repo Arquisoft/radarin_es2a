19011011fce17485896a61e9b9219d6d7d62205c
uo245020 <uo245020@uniovi.es>
2021-04-11 15:12:08 +0200

    Prueba despliegue

---
+++ webapp/e2e/steps/register-form.steps.js
@@ -0,0 +1,37 @@
+const {defineFeature, loadFeature}=require('jest-cucumber');
+const feature = loadFeature('./features/register-form.feature');
+
+defineFeature(feature, test => {
+
+  beforeEach(async () => {
+    await global.page.goto('http://localhost:3000')
+  })
+
+  test('The user is not registered in the site', ({given,when,then}) => {
+       given('An unregistered user', () => {
+
+    	});
+
+    	when('I fill the data in the form and press submit', () => {
+
+    	});
+
+    	then('A welcome message should be shown in the screen', () => {
+
+    	});
+  });
+
+  test('The user is already registered in the site', ({ given, when, then }) => {
+    
+    given('An already registered user', () => {
+     });
+ 
+     when('I fill the data in the form and press submit',  () => {
+       
+     });
+ 
+     then('An error message should be shown in the screen',  () => {
+     });
+     
+   });
+});
