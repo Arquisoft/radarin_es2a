6e524443e311512d57360c076fbe947e1c9655c5
uo245020 <uo245020@uniovi.es>
2021-04-22 19:51:32 +0200

    Prueba despliegue

--- webapp/server.js
+++ webapp/server.js
@@ -3,3 +3,6 @@ var app = express()
 var port =  process.env.PORT || 3000
 app.use(express.static('build'))
 app.listen(port)
+app.get('*' , (req, res) => {
+    res.sendFile(path.join(__dirname,'build','index.html')); 
+});
