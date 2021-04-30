a1ec5470b20c4c8b31eee11ed5f23c0b6b12a266
Lucas <UO264423@uniovi.es>
2021-04-30 02:50:49 +0200

    App asks for location <3

--- RadarinAndroid/app/src/main/java/com/example/radarinandroid/MainActivity.java
+++ RadarinAndroid/app/src/main/java/com/example/radarinandroid/MainActivity.java
@@ -1,28 +1,118 @@
 package com.example.radarinandroid;
 
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
-    private WebView webView;
+    private final int MY_PERMISSIONS_REQUEST_LOCATION = 1;
+
+    public static WebView myWebView;
+    private String url = "https://radarines2awebapp.herokuapp.com/";
+
+    private String mGeoLocationRequestOrigin;
+    private GeolocationPermissions.Callback mGeoLocationCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        webView = (WebView) findViewById(R.id.webview);
+        myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl(url);
 
-
-        WebSettings webSettings = webView.getSettings();
+        WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        //webSettings.setSupportMultipleWindows(true);
 
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://radarines2awebapp.herokuapp.com");
+        askLocationPermission();
 
+        myWebView.setWebViewClient(new WebViewClient());
+    }
+
+    private void askLocationPermission() {
+        Activity that = this;
+        myWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+                mGeoLocationRequestOrigin = null;
+                mGeoLocationCallback = null;
+                // Do We need to ask for permission?
+                if (ContextCompat.checkSelfPermission(
+                        getApplicationContext(),
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED) {
+                    mGeoLocationRequestOrigin = origin;
+                    mGeoLocationCallback = callback;
+                    ActivityCompat.requestPermissions(
+                            that,
+                            new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                            MY_PERMISSIONS_REQUEST_LOCATION);
+                } else {
+                    // Tell the WebView that permission has been granted
+                    callback.invoke(origin, true, false);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length != 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted
+                    mGeoLocationCallback.invoke(mGeoLocationRequestOrigin, true, false);
+                } else {
+
+                    // permission denied
+                    // Disable the functionality that depends on this permission.
+                    mGeoLocationCallback.invoke(mGeoLocationRequestOrigin, false, false);
+                }
+                break;
+            }
+            default:
+                break;
+        }
+        // other 'case' lines to check for other
+        // permissions this app might request
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        WebView webView = (WebView) findViewById(R.id.webview);
+
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch(keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (webView.canGoBack()) {
+                        webView.goBack();
+                    } else {
+                        finish();
+                    }
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        return super.onKeyDown(keyCode, event);
     }
 }
