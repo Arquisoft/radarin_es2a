6cb81deaea203eed4c2ef782b36748ce8eb9c76c
uo245020 <uo245020@uniovi.es>
2021-05-03 01:16:04 +0200

    Prueba encontrar test falla-3

---
+++ webapp/src/tests/Home.test.js
@@ -0,0 +1,13 @@
+import { cleanup } from "@testing-library/react";
+import React from "react";
+import ReactDOM from "react-dom";
+import Home from "../components/Home";
+
+afterAll(cleanup);
+describe("<Peticiones />", () => {
+
+    it("renders without crashing", () => {
+        const div = document.createElement("div");
+        ReactDOM.render(<Home />, div);
+    });
+  });
