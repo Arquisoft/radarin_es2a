a83df2b3b5e4bd3e6ec70a37e78eea9e19d5fc89
uo245020 <uo245020@uniovi.es>
2021-05-03 09:36:07 +0200

    Prueba cambio test

--- webapp/src/tests/Peticiones.test.js
+++ webapp/src/tests/Peticiones.test.js
@@ -1,13 +1,28 @@
 import React from "react";
-import {  cleanup } from "@testing-library/react";
+import {  render, cleanup } from "@testing-library/react";
 import ReactDOM from "react-dom";
 import Peticiones from "../components/Friends/Peticiones";
+import { BrowserRouter } from "react-router-dom";
 
 afterAll(cleanup);
-describe("<Peticiones />", () => {
-
-    it("renders without crashing", () => {
-        const div = document.createElement("div");
-        ReactDOM.render(<Peticiones />, div);
-    });
+test("Peticiones con usuario normal", async() => {
+    jest.useFakeTimers();
+    window.sessionStorage.setItem("user","uo271314@uniovi.es");
+    render(
+      <BrowserRouter>
+    <Peticiones />
+    </BrowserRouter>
+    );
+    jest.advanceTimersByTime(1000);
   });
+
+  test("Peticiones sin usuario", async() => {
+    jest.useFakeTimers();
+    window.sessionStorage.removeItem("user")
+    render(
+      <BrowserRouter>
+    <Peticiones />
+    </BrowserRouter>
+    );
+    jest.advanceTimersByTime(1000);
+  }); 
